7777b8e1cfd619fe835a687204597fe93fc1a5a2
Chris Leech <cleech@redhat.com>
2024-12-12 05:40:42 -0800

    move grid code to a util mod

---
+++ src/util/grid.rs
@@ -0,0 +1,75 @@
+use super::point::Point;
+
+use std::fmt::Debug;
+use std::ops::{Index, IndexMut};
+use std::str::FromStr;
+
+pub struct Grid<T> {
+    pub width: isize,
+    pub height: isize,
+    raw: Vec<T>,
+}
+
+impl Grid<u8> {
+    pub fn _raw(input: &str) -> Self {
+        let bytes: Vec<_> = input.lines().map(str::as_bytes).collect();
+        let height = bytes.len() as isize;
+        let width = bytes[0].len() as isize;
+        let mut raw = Vec::with_capacity((width * height) as usize);
+        bytes.iter().for_each(|slice| raw.extend_from_slice(slice));
+        Grid { width, height, raw }
+    }
+}
+
+impl<T: FromStr> Grid<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    pub fn parse(input: &str) -> Self {
+        let v: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+        let height = v.len() as isize;
+        let width = v[0].len() as isize;
+        let data = v
+            .into_iter()
+            .flat_map(|l| l.into_iter().map(|c| c.to_string().parse::<T>().unwrap()))
+            .collect();
+        Grid {
+            width,
+            height,
+            raw: data,
+        }
+    }
+}
+
+impl<T> Grid<T> {
+    pub fn map<B, F>(&self, f: F) -> Grid<B>
+    where
+        F: FnMut(&T) -> B,
+    {
+        let raw: Vec<B> = self.raw.iter().map(f).collect();
+        Grid {
+            width: self.width,
+            height: self.height,
+            raw,
+        }
+    }
+
+    pub fn in_bounds(&self, (x, y): Point) -> bool {
+        x >= 0 && x < self.width && y >= 0 && y < self.height
+    }
+}
+
+impl<T> Index<Point> for Grid<T> {
+    type Output = T;
+    #[inline]
+    fn index(&self, index: Point) -> &Self::Output {
+        &self.raw[(self.width * index.1 + index.0) as usize]
+    }
+}
+
+impl<T> IndexMut<Point> for Grid<T> {
+    #[inline]
+    fn index_mut(&mut self, index: Point) -> &mut Self::Output {
+        &mut self.raw[(self.width * index.1 + index.0) as usize]
+    }
+}
---
+++ src/util/point.rs
@@ -0,0 +1,1 @@
+pub type Point = (isize, isize);
