a240216488a084cca9828b5057708ca2eb478122
Chris Leech <cleech@redhat.com>
2024-12-18 14:21:09 -0800

    day 17 part 2

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -2,16 +2,15 @@ advent_of_code::solution!(17);
 
 use itertools::Itertools;
 use sscanf::scanf;
-use std::collections::VecDeque;
 
 #[derive(Debug, Clone)]
 struct Cpu {
-    a: usize,
-    b: usize,
-    c: usize,
+    a: u64,
+    b: u64,
+    c: u64,
     ip: usize,
     program: Vec<u8>,
-    output: VecDeque<u8>,
+    output: Vec<u8>,
 }
 
 #[derive(Debug)]
@@ -28,61 +27,74 @@ enum Opcode {
 use Opcode::*;
 
 impl Cpu {
-    fn decode(opcode: u8, operand: u8) -> Opcode {
+    fn run_one(&mut self) -> Option<u8> {
+        let combo = |operand: u8| -> u64 {
+            match operand {
+                n @ 0..=3 => n.into(),
+                4 => self.a,
+                5 => self.b,
+                6 => self.c,
+                _ => unreachable!(),
+            }
+        };
+        let decode = |opcode: u8, operand: u8| -> Opcode {
+            match opcode {
+                0 => Adv(operand),
+                1 => Bxl(operand),
+                2 => Bst(operand),
+                3 => Jnz(operand),
+                4 => Bxc,
+                5 => Out(operand),
+                6 => Bdv(operand),
+                7 => Cdv(operand),
+                _ => unreachable!(),
+            }
+        };
+        let opcode = decode(self.program[self.ip], self.program[self.ip + 1]);
         match opcode {
-            0 => Adv(operand),
-            1 => Bxl(operand),
-            2 => Bst(operand),
-            3 => Jnz(operand),
-            4 => Bxc,
-            5 => Out(operand),
-            6 => Bdv(operand),
-            7 => Cdv(operand),
-            _ => unreachable!(),
-        }
-    }
-    fn combo(&self, operand: u8) -> Option<usize> {
-        match operand {
-            n @ 0..=3 => Some(n.into()),
-            4 => Some(self.a),
-            5 => Some(self.b),
-            6 => Some(self.c),
-            7 => None,
-            _ => unreachable!(),
-        }
-    }
-    fn process(&mut self) -> Option<()> {
-        let opcode = Self::decode(self.program[self.ip], self.program[self.ip + 1]);
-        // println!("{:?}", opcode);
-        match opcode {
-            Adv(combo) => self.a >>= self.combo(combo)?,
-            Bxl(literal) => self.b ^= literal as usize,
-            Bst(combo) => self.b = self.combo(combo)? % 8,
+            Adv(operand) => self.a >>= combo(operand),
+            Bxl(literal) => self.b ^= literal as u64,
+            Bst(operand) => self.b = combo(operand) % 8,
             Jnz(literal) => {
                 if self.a != 0 {
                     self.ip = literal as usize;
-                    return Some(());
+                    return None;
                 }
             }
             Bxc => self.b ^= self.c,
-            Out(combo) => {
-                let out = (self.combo(combo)? % 8).try_into().unwrap();
-                self.output.push_back(out);
+            Out(operand) => {
+                let out = (combo(operand) % 8).try_into().unwrap();
+                self.ip += 2;
+                return Some(out);
             }
-            Bdv(combo) => self.b = self.a >> self.combo(combo)?,
-            Cdv(combo) => self.c = self.a >> self.combo(combo)?,
+            Bdv(operand) => self.b = self.a >> combo(operand),
+            Cdv(operand) => self.c = self.a >> combo(operand),
         }
         self.ip += 2;
-        Some(())
+        None
+    }
+    fn run_to_next_output(&mut self) -> Option<u8> {
+        while self.ip < self.program.len() - 1 {
+            let out = self.run_one();
+            if out.is_some() {
+                return out;
+            }
+        }
+        None
+    }
+    fn run_to_completion(&mut self) {
+        while let Some(out) = self.run_to_next_output() {
+            self.output.push(out);
+        }
     }
 }
 
 fn parse(input: &str) -> Option<Cpu> {
     let (prefix, suffix) = input.split_once("\n\n")?;
     let mut registers = prefix.lines();
-    let a = scanf!(registers.next()?, "Register A: {usize}").ok()?;
-    let b = scanf!(registers.next()?, "Register B: {usize}").ok()?;
-    let c = scanf!(registers.next()?, "Register C: {usize}").ok()?;
+    let a = scanf!(registers.next()?, "Register A: {u64}").ok()?;
+    let b = scanf!(registers.next()?, "Register B: {u64}").ok()?;
+    let c = scanf!(registers.next()?, "Register C: {u64}").ok()?;
     let suffix = suffix.trim();
     let p = scanf!(suffix, "Program: {}", str).unwrap();
     let p = p
@@ -95,45 +107,40 @@ fn parse(input: &str) -> Option<Cpu> {
         c,
         ip: 0,
         program: p,
-        output: VecDeque::default(),
+        output: Vec::default(),
     })
 }
 
 pub fn part_one(input: &str) -> Option<String> {
     let mut cpu = parse(input)?;
-    while cpu.ip < cpu.program.len() - 1 {
-        cpu.process()?;
-    }
-    // println!("{:?}", cpu.output);
+    cpu.run_to_completion();
     Some(cpu.output.into_iter().map(|n| n.to_string()).join(","))
 }
 
-pub fn part_two(input: &str) -> Option<usize> {
+pub fn part_two(input: &str) -> Option<u64> {
+    /* All of these functions operate on an input in register A, and loop until it's reduced to 0.
+     * Each loop outputs a 3-bit value, derivied from A. Knowing that A is reduced by 3-bits each
+     * pass, and that the low 3-bits determine the output, we can solve this 3-bits at a time.
+     */
     let cpu = parse(input)?;
-    for a in 0..usize::MAX {
-        let mut cpu = cpu.clone();
-        cpu.a = a;
-        while cpu.ip < cpu.program.len() - 1 {
-            if !cpu.process().is_some() {
-                break;
-            }
-            if cpu.output.len() > cpu.program.len() {
-                break;
-            }
-        }
-        let output: Vec<u8> = cpu.output.into();
+    println!("{:?}", cpu.program);
+    _part_two(&cpu, cpu.program.len(), 0)
+}
 
-        if cpu.program.len() == output.len() {
-            let o = output.iter().map(|n| n.to_string()).join(",");
-            let p = cpu.program.iter().map(|n| n.to_string()).join(",");
-            if o == p {
-                println!("({}) o: {}", a, o);
-                println!("({}) p: {}", a, p);
-                return Some(a);
+fn _part_two(cpu: &Cpu, depth: usize, a: u64) -> Option<u64> {
+    if depth == 0 {
+        return Some(a);
+    }
+    for rem in 0..8 {
+        let mut c = cpu.clone();
+        c.a = (a << 3) | rem;
+        let o = c.run_to_next_output().unwrap();
+        if o == cpu.program[depth - 1] {
+            if let Some(_a) = _part_two(&cpu, depth - 1, (a << 3) | rem) {
+                return Some(_a);
             }
         }
     }
-    // println!("{:?}", cpu.output);
     None
 }
 
